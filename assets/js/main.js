78c07f9c0945e7ff9216f6e1fd37c667f03cdfa0
Leo Alves <leoalves0698@gmail.com>
2023-02-01 19:04:14 -0300

    label: tirando bandeira de países fora da copa e adicionando dinamismo ao card com js

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+function createGame (playerOne, hour, playerTwo) {
+    return `
+        <li>
+            <img src="./assets/img/bandeiras-dos-países/icon-${playerOne}.svg" alt="Bandeira da ${playerOne}">
+            <strong>${hour}</strong>
+            <img src="./assets/img/bandeiras-dos-países/icon-${playerTwo}.svg" alt="Bandeira do ${playerTwo}">
+        </li>
+    `
+}
+
+function createCard (date, day, games) {
+    return `
+        <div class="card">
+            <h2>${date} <span>${day}</span></h2>
+            <ul>
+                ${games}
+            </ul>
+        </div>
+    `
+}
+
+document.querySelector("#app").innerHTML = `
+    <header class="header">
+        <img src="./assets/img/logo.svg" alt="Logo da NLW">
+        <img src="./assets/img/calendario.svg" alt="calendário">
+    </header>
+
+    <main id="cards">
+        ${createCard("24/11", "quinta", createGame("switzerland", "16:00", "cameroon") + createGame("portugal", "12:00", "ghana") + createGame("brazil", "16:00", "serbia"))} 
+        ${createCard("28/11", "segunda", createGame("south-korea", "10:00", "ghana")+ createGame("brazil", "13:00", "switzerland")+ createGame("portugal", "16:00", "uruguay"))}
+        
+    </main>
+`
