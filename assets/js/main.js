b8d0dd5c610a26b5323a95bf404b92ab5a0bc03c
Leo Alves <leoalves0698@gmail.com>
2023-02-02 17:19:53 -0300

    label: refatoração do código e animação adicionada

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,11 @@ function createGame (playerOne, hour, playerTwo) {
     `
 }
 
+let delay = -0.3
 function createCard (date, day, games) {
+    delay = delay + 0.3
     return `
-        <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
             <h2>${date} <span>${day}</span></h2>
             <ul>
                 ${games}
@@ -20,5 +22,9 @@ function createCard (date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML = 
-        createCard("24/11", "quinta", createGame("switzerland", "16:00", "cameroon") + createGame("portugal", "12:00", "ghana") + createGame("brazil", "16:00", "serbia")) + 
-        createCard("28/11", "segunda", createGame("south-korea", "10:00", "ghana")+ createGame("brazil", "13:00", "switzerland")+ createGame("portugal", "16:00", "uruguay"))
+        createCard("24/11", "quinta", createGame("switzerland", "10:00", "cameroon") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia")) 
+        + 
+        createCard("28/11", "segunda", createGame("south-korea", "10:00", "ghana")+ createGame("brazil", "13:00", "switzerland")+ createGame("portugal", "16:00", "uruguay")) 
+        +
+        createCard("02/12", "sexta", createGame("ghana", "10:00", "uruguay")+ createGame("brazil", "13:00", "cameroon")+ createGame("portugal", "16:00", "south-korea"))
+
